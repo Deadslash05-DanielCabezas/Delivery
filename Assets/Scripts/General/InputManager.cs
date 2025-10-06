37facaedd9ceaaa8c35f5187cc47f7a266ec9074
Deadslash05 <daniel.cabezas.c11@gmail.com>
2025-10-06 15:15:13 +0200

    turning

--- Assets/Scripts/General/InputManager.cs
+++ Assets/Scripts/General/InputManager.cs
@@ -8,6 +8,9 @@ public class InputManager : MonoBehaviour
     private PlayerMovement playerMovement;
 
     private InputAction moveAction;
+
+    private Vector2 moveDirection;
+    private Vector2 oldDirection = new Vector2(0, 0);
     private void Start()
     {
         playerMovement = player.GetComponent<PlayerMovement>();
@@ -17,7 +20,29 @@ public class InputManager : MonoBehaviour
 
     private void Update()
     {
-        if (moveAction.IsPressed())
+
+        if (moveAction.inProgress)
             playerMovement.accelerate();
+
+        if (moveAction.triggered)
+        {
+            moveDirection = moveAction.ReadValue<Vector2>();
+
+            if (moveDirection.x != oldDirection.x)
+            {
+                oldDirection = moveDirection;
+                if (moveDirection.x == -1)
+                    playerMovement.turn(-90);
+                else if (moveDirection.x == 1)
+                    playerMovement.turn(90);
+                else
+                    playerMovement.stopTurning();
+            }
+        }
+
+        if (moveAction.ReadValue<Vector2>() == Vector2.zero)
+        {
+            playerMovement.stopTurning();
+        }    
     }
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,23 +1,39 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour 
 {
     private Rigidbody rb;
+    private Transform t;
 
     [SerializeField] private float speed;
     [SerializeField] private float maxSpeed = 600;
     [SerializeField] private float acceleration = 4;
     [SerializeField] private float deceleration = 1;
+    [SerializeField] private float turnSpeed = 5;
+
+    private float targetRotation;
+    private float currentRotation;
+    private float newRotation;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        t = GetComponent<Transform>();
     }
 
     private void Update()
     {
+        currentRotation = t.rotation.y;
+        if (!Mathf.Approximately(currentRotation, targetRotation))
+        {
+            print("a");
+            newRotation = Mathf.MoveTowardsAngle(currentRotation, targetRotation, turnSpeed);
+            t.rotation = Quaternion.Euler(0, newRotation, 0);
+        }
+
         //Update velocity
-        rb.linearVelocity = speed * Vector3.forward;
+        rb.linearVelocity = speed * t.forward;
         speed -= deceleration;
         if (speed < 0)
             speed = 0;
@@ -28,5 +44,17 @@ public class PlayerMovement : MonoBehaviour
         speed += acceleration;
         if (speed >  maxSpeed)
             speed = maxSpeed;
+    }
+
+    public void turn(int angle)
+    {
+        print("c");
+        targetRotation = t.rotation.y + angle;
+    }
+
+    public void stopTurning()
+    {
+        print("d");
+        targetRotation = t.rotation.y;
     }
 }
